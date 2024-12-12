f1910b3ef6bb2ac1b4a2c0904660aeb074ff430b
daniellin54057 <102565685+daniellin54057@users.noreply.github.com>
2024-12-12 15:53:56 +0800

    Update paddle.py
    加入使球拍變短的部分，用於訓練模型準確度
    並加入reward>某特定值及停止的設計，控制時間

--- paddle.py
+++ paddle.py
@@ -1,17 +1,19 @@
 import pygame
-
+import config
 BLACK = (0, 0, 0)
 
 class Paddle(pygame.sprite.Sprite):
     
     def __init__(self, color, width, height):
         super().__init__()
-        
-        self.image = pygame.Surface([width, height])
+        self.width = width
+        self.height = height
+        self.image = pygame.Surface([self.width, self.height])
         self.image.fill(BLACK)
         self.image.set_colorkey(BLACK)
  
-        pygame.draw.rect(self.image, color, [0, 0, width, height], border_radius=0)
+        pygame.draw.rect(self.image, color, [0, 0, self.width, self.height], border_radius=0)
+        
         
         self.rect = self.image.get_rect()
 
@@ -25,5 +27,8 @@ class Paddle(pygame.sprite.Sprite):
           
     def moveDown(self, pixels):
         self.rect.y += pixels
-        if self.rect.y > 400:
-            self.rect.y = 400
+        if self.rect.y > config.SCREEN_HEIGHT - self.height:
+            self.rect.y = config.SCREEN_HEIGHT - self.height
+    def changehight(self, pixels):
+        self.height = pixels
+        
