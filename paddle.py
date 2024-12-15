e2e7c24f163fb3755425f2ea7b3654126d1ac25a
daniellin54057 <102565685+daniellin54057@users.noreply.github.com>
2024-12-15 21:10:42 +0800

    Update paddle.py
    Change hight

--- paddle.py
+++ paddle.py
@@ -6,6 +6,8 @@ class Paddle(pygame.sprite.Sprite):
     
     def __init__(self, color, width, height):
         super().__init__()
+        self.color = color
+        self.width = width
         
         self.image = pygame.Surface([width, height])
         self.image.fill(BLACK)
@@ -27,3 +29,12 @@ class Paddle(pygame.sprite.Sprite):
         self.rect.y += pixels
         if self.rect.y > 400:
             self.rect.y = 400
+    def changehight(self, pixels):
+        self.height = pixels
+        self.image = pygame.Surface([self.width, self.height])
+        self.image.fill(BLACK)
+        self.image.set_colorkey(BLACK)
+        pygame.draw.rect(self.image, self.color, [0, 0, self.width, self.height], border_radius=0)        
+        self.rect = self.image.get_rect()
+        # 創建mask，精確檢查碰撞
+        self.mask = pygame.mask.from_surface(self.image)
